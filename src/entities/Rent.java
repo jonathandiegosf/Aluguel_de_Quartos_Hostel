6ea55c12129796fc2da4ba0cbaac64bd8430968a
Jonathan Diego <jonathan_diego6@hotmail.com>
2023-01-24 15:00:12 -0300

    Aluguel do Quartos no Hostel

---
+++ src/entities/Rent.java
@@ -0,0 +1,47 @@
+package entities;
+
+public class Rent {
+    String name;
+    String email;
+    Integer room;
+
+    public Rent() {
+
+    }
+
+    public Rent(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getRoom() {
+        return room;
+    }
+
+    public void setRoom(Integer room) {
+        this.room = room;
+    }
+
+    public String toString() {
+        return name
+                + ", "
+                + email;
+    }
+
+}
